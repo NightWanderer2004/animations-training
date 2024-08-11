6b18f9c9462d762531996241bb7eaaac5cf879ad
Eduard Povierin <edpovieri@gmail.com>
2024-08-11 18:45:30 +0200

    Fix mobile view

--- src/app/magnetic-btns/page.js
+++ src/app/magnetic-btns/page.js
@@ -4,15 +4,15 @@ import { motion, useMotionValue, useSpring } from 'framer-motion'
 
 export default function page() {
    return (
-      <div className='flex gap-6 -mt-[500px] max-w-fit mx-auto rounded-2xl py-5 px-6 border border-black/10 scale-150'>
-         <MagneticBtn>Open</MagneticBtn>
-         <MagneticBtn>Share link ↗</MagneticBtn>
-         <MagneticBtn>← Back</MagneticBtn>
+      <div className='grid grid-cols-2 grid-rows-2 lg:flex gap-3 lg:gap-6 -mt-[500px] max-w-fit mx-auto rounded-2xl py-5 px-6 border border-black/10 lg:scale-150'>
+         <MagneticBtn className='col-span-1 lg:col-span-1'>Open</MagneticBtn>
+         <MagneticBtn className='col-span-1 lg:col-span-1'>Share link ↗</MagneticBtn>
+         <MagneticBtn className='col-span-2 lg:col-span-1'>← Back</MagneticBtn>
       </div>
    )
 }
 
-function MagneticBtn({ children }) {
+function MagneticBtn({ children, className }) {
    const ref = useRef(null)
    const springConf = { stiffness: 150, damping: 15, mass: 0.1 }
    const position = { x: useMotionValue(0), y: useMotionValue(0) }
@@ -35,7 +35,7 @@ function MagneticBtn({ children }) {
    const { x, y } = positionSpring
 
    return (
-      <button className='px-6 py-2 text-neutral-900 relative group active:scale-[98%] transition-transform'>
+      <button className={`px-6 py-2 text-neutral-900 relative group active:scale-[98%] transition-transform ${className}`}>
          <motion.div
             ref={ref}
             className='absolute top-0 left-0 bg-black/5 group-active:bg-black/10 text-neutral-900 rounded-xl w-full h-full transition-colors'
