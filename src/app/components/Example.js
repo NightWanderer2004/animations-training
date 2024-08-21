c95b218996cc30a306ef25342923c3f2d0bd4f43
Eduard Povierin <edpovieri@gmail.com>
2024-08-21 11:41:33 +0200

    Update 2

--- src/app/components/Example.js
+++ src/app/components/Example.js
@@ -1,15 +1,20 @@
 import { motion } from 'framer-motion'
 import { e_ukraine } from '../fonts'
 import Image from 'next/image'
+import useMediaQuery from '@/hooks/useMediaQuery'
 
-export default function Example({ delay, title, link, category }) {
+export default function Example({ containerRef, delay, title, link, category }) {
+   const isMobile = useMediaQuery('(max-width: 640px)')
+
    return (
       <motion.a
          href={`/examples/${link}`}
-         initial={{ opacity: 0, y: 40, scale: 0.98 }}
-         animate={{ opacity: 1, y: 0, scale: 1 }}
-         transition={{ duration: 0.9, delay: 0.4 + delay, ease: 'backOut' }}
-         className={`w-full h-[430px] lg:h-[340px] cursor-pointer ${e_ukraine.className}`}
+         initial={{ opacity: 0, y: 20, scale: 0.98 }}
+         whileInView={{ opacity: 1, y: 0, scale: 1 }}
+         whileTap={{ scale: 0.98 }}
+         viewport={{ once: isMobile ? true : false, root: containerRef, margin: `0px 0px ${isMobile ? -160 : -200}px 0px` }}
+         transition={{ duration: 0.9, delay: 0.3 + delay, ease: 'backOut' }}
+         className={`w-full h-[430px] lg:h-[320px] cursor-pointer ${e_ukraine.className}`}
       >
          <motion.div className='relative h-full'>
             <div className='absolute top-0 left-0 block w-full h-full bg-blue-50 rounded-lg' />
@@ -18,7 +23,7 @@ export default function Example({ delay, title, link, category }) {
                width={300}
                height={200}
                alt={title}
-               className='relative z-10 p-1.5 h-full w-full rounded-xl object-cover'
+               className='relative z-10 p-1 h-full w-full rounded-xl object-cover pointer-events-none'
             />
          </motion.div>
          <span className='block text-neutral-400/75 text-xs pt-1'>{category}</span>
--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,30 @@ import { dm_serif_display } from './fonts'
 import { Heading } from './components/Heading'
 import Example from './components/Example'
 import { links } from './data'
+import { useRef } from 'react'
 
 export default function Home() {
+   const container = useRef(null)
+
    return (
-      <div className={`${dm_serif_display.className}`}>
+      <div className={`relative m-2 lg:m-4 ${dm_serif_display.className}`}>
          <Heading text='Welcome to Ani-Magic' />
-         <div className='relative z-20 container max-w-6xl mx-auto px-2 lg:px-0 pt-[70vh] pb-20 lg:pt-[90vh] grid lg:grid-cols-3 gap-20 lg:gap-x-8 gap-y-16'>
+         <div
+            ref={container}
+            className='relative z-20 container max-w-6xl mx-auto px-2 lg:px-0 pt-[70vh] pb-20 lg:pt-[90vh] grid lg:grid-cols-3 gap-20 lg:gap-x-8 gap-y-20'
+         >
             {links.map((el, i) => {
                const delay = i * 0.075
-               return <Example key={i} delay={delay} title={el.link.split('-').join(' ')} link={el.link} category={el.category} />
+               return (
+                  <Example
+                     key={i}
+                     containerRef={container}
+                     delay={delay}
+                     title={el.link.split('-').join(' ')}
+                     link={el.link}
+                     category={el.category}
+                  />
+               )
             })}
          </div>
       </div>
