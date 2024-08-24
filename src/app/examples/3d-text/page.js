ebf7457895709f197be2f0561c1fdfdb211c9be0
Eduard Povierin <edpovieri@gmail.com>
2024-08-24 21:00:20 +0200

    Add wrapper

--- src/app/examples/3d-text/page.js
+++ src/app/examples/3d-text/page.js
@@ -2,18 +2,21 @@
 import { useRef } from 'react'
 import s from './style.module.scss'
 import { dm_serif_display } from '@/app/fonts'
+import PageWrapper from '@/app/components/PageWrapper'
 
 export default function page() {
    const plane = useRef(null)
 
    return (
-      <div className={`${s.container} ${dm_serif_display.className}`}>
-         <div ref={plane} className={s.body}>
-            <Text3d primary={'Beautiful'} secondary={'Beautiful'} />
-            <Text3d primary={'space'} secondary={'space'} />
-            <Text3d primary={'to create'} secondary={'to create'} />
+      <PageWrapper>
+         <div className={`${s.container} ${dm_serif_display.className}`}>
+            <div ref={plane} className={s.body}>
+               <Text3d primary={'Beautiful'} secondary={'Beautiful'} />
+               <Text3d primary={'space'} secondary={'space'} />
+               <Text3d primary={'to create'} secondary={'to create'} />
+            </div>
          </div>
-      </div>
+      </PageWrapper>
    )
 }
 
